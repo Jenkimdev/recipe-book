69033f83934546269f9038d145566777366486b9
Mitchell Jenkins <mitchelljenkins1999@gmail.com>
2024-02-10 12:15:29 +0000

    Update to set debug to false for security

--- app.py
+++ app.py
@@ -165,5 +165,5 @@ def search():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
     recipes = list(mongo.db.recipes.find())
